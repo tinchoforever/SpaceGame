8b09b4e262c9dac88d1185cf05f16a224019ce96
Tom <tomasloon@gmail.com>
2012-04-21 20:34:14 -0300

    Demo views

--- view/js/main.js
+++ view/js/main.js
@@ -1,7 +1,56 @@
-(function($){
+var NASA = {};
+
+(function($, App){
 	
-	$('.satellite-checkin-list').on('click', 'tr', function(){
-		window.location.href = $(this).data('href');
-	});
+	var toKM = function(km) {
+		return (km).toFixed(1) + 'km';
+	};
 	
-}(jQuery));
+	App.checkin = function(){
+		var list = $('.satellite-checkin-list'),
+			template = list.html(),
+			markup = [];
+		if (!list || !template){
+			return false;
+		}
+			
+		var parseSatellites = function(satellites) {
+			satellites.sort(function(a, b) {
+				return a.distance - b.distance;
+			});
+			
+			for (var i = 0, item; item = satellites[i++]; ) {
+				markup.push(
+					template.replace('{{name}}', (item.name || item.satId))
+							.replace('{{distance}}', toKM(item.distance))
+							.replace('{{id}}', item.satId)
+				);
+				// item.cat;
+			}
+		};
+		
+		var build = function(){
+			list.html( markup.join('') ).removeClass('hide');
+			
+			list.on('click', 'tr', function(){
+				window.location.href = $(this).data('href');
+			});
+		};
+		
+		console.log('Requesting...');
+		$.get('http://sockets.brunolazzaro.com.ar/near/-34/-58', function(r) {
+			console.log('Response received.');
+			if (r[0].error) {
+				// fuck it
+				alert('Failure');
+			} else {
+				parseSatellites(r);
+				build();
+			}
+		});
+	};
+	
+	App.checkin();
+	
+	
+}(jQuery, NASA));
